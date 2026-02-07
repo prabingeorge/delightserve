b1335a5968ed0a915f2ff9d45cf929f793f79659
prabingeorge <prabingeorge@gmail.com>
2026-02-07 18:15:24 +0530

    Changed the code

--- API/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const admin = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Access denied: Admins only" });
-  }
-  next();
-};
-
-export default admin;
